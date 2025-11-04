13cbe35000623e56db833f8a5afa24e3234cf807
ElnuDev <elnu@elnu.com>
2025-11-04 14:17:06 -0800

    Remove old debug print

--- options.js
+++ options.js
@@ -1,7 +1,6 @@
 const storage = "browser" in window ? browser.storage : chrome.storage;
 
 async function loadState() {
-	console.log("we're loading!");
 	for (const [key, value] of Object.entries(await storage.sync.get())) {
 		const elements = document.body.querySelectorAll(`[name=${key}]`);
 		const apply = typeof value === "boolean" ? el => el.checked = value : el => el.value = value;
